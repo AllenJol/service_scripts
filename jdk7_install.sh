77d07cbe42644e4b121942a58c39bab08bb2c8fe
AllenJol <471733951@qq.com>
2018-03-27 16:25:59 +0800

    jdk-1.8和jdk-1.7安装脚本

--- jdk7_install.sh
+++ jdk7_install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#__Author__:Allen_Jol at 2018-03-27 19:13:17
-#Description: jdk8 install
+#__Author__:Allen_Jol at 2018-03-27 15:40:04
+#Description: jkd 7 install
 
-JDK8_VERSION="jdk-8u141-linux-x64.tar.gz"
+JDK7_VERSION="jdk1.7.0_79"
+JDK7_DOWN_URL="http://ozi3kq0eb.bkt.clouddn.com/${JDK7_VERSION}.tar.gz"
 
 function check_root(){
   if [ $UID -ne 0 ];then
@@ -15,7 +16,8 @@ function check_dir(){
 if [ ! -d "/usr/local/java" ];then
   mkdir -p /usr/local/java
 else
-  echo "dir have exsits."
+  echo "dir have exsits and locked,please check..."
+  exit 1
 fi
 }
 
@@ -29,18 +31,18 @@ function check_jdk(){
   fi
 }
 
-function jdk8_down_install(){
-  NETSTAT="ping -c 1 www.baidu.com >>/dev/null"
+function jdk7_down_install(){
+    NETSTAT="ping -c 1 www.baidu.com >>/dev/null"
   if [ $? -ne 0 ];then
     echo "network is error,please check..."
   else
     echo "network is ok,download ${JDK8_VERSION} now.please wait for a moment."
     cd /usr/local/java 
-    wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz" 
-    tar -zxf ${JDK8_VERSION} -C /usr/local/java && ln -s /usr/local/java/jdk1.8.0_141 /usr/jdk
+    wget -c ${JDK7_DOWN_URL}
+    tar -zxf ${JDK7_VERSION}.tar.gz -C /usr/local/java && ln -s /usr/local/java/jdk1.7.0_79 /usr/jdk
     #rpm -ivh ${JDK8_VERSION} && ln -s /usr/local/java/${JDK8_VERSION}  /usr/jdk
   fi
-echo 'export JAVA_HOME=/usr/local/java/jdk1.8.0_141' >>/etc/profile
+echo 'export JAVA_HOME=/usr/local/java/jdk1.7.0_79' >>/etc/profile
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >>/etc/profile
 echo 'export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar' >>/etc/profile
 source /etc/profile
@@ -50,6 +52,6 @@ function main(){
   check_root
   check_dir
   check_jdk
-  jdk8_down_install
+  jdk7_down_install
 }
 main
