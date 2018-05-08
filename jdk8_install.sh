c08dccb8c7197d0f548e8f8f720b07fa74f6f1af
AllenJol <471733951@qq.com>
2018-05-08 11:39:04 +0800

    modify_update jdk8_install.sh

--- jdk8_install.sh
+++ jdk8_install.sh
@@ -12,19 +12,21 @@ function check_root(){
 }
 
 function check_dir(){
-if [ ! -d "/usr/local/java" ];then
-  mkdir -p /usr/local/java
+if [ ! -d "/usr/java" ];then
+  mkdir -p /usr/java
 else
-  echo "dir have exsits."
+  echo -e "\033[1;31java dir have exsits,and scripts has locked.\033[0m"
+  exit 1
 fi
 }
 
 function check_jdk(){
  JAVA=`java -version | wc -l`
   if [ ${JAVA} -eq 0 ] ;then
-    echo "There is no java jdk installed."
+    echo -e "\033[1;31mIgnore Error about [ java command not found ]\033[0m"
+	echo -e "\033[1;33mThere is no java jdk installed.\033[0m"
   else
-    echo "There have installed java jdk,please check..."
+    echo -e "\033[1;33mThere have installed java jdk,please check...\033[0m"
     exit 1
   fi
 }
@@ -35,12 +37,12 @@ function jdk8_down_install(){
     echo "network is error,please check..."
   else
     echo "network is ok,download ${JDK8_VERSION} now.please wait for a moment."
-    cd /usr/local/java 
+    cd /usr/java 
     wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u141-b15/336fa29ff2bb4ef291e347e091f7f4a7/jdk-8u141-linux-x64.tar.gz" 
-    tar -zxf ${JDK8_VERSION} -C /usr/local/java && ln -s /usr/local/java/jdk1.8.0_141 /usr/jdk
+    tar -zxf ${JDK8_VERSION} -C /usr/java && ln -s /usr/java/jdk1.8.0_141 /usr/jdk
     #rpm -ivh ${JDK8_VERSION} && ln -s /usr/local/java/${JDK8_VERSION}  /usr/jdk
   fi
-echo 'export JAVA_HOME=/usr/local/java/jdk1.8.0_141' >>/etc/profile
+echo 'export JAVA_HOME=/usr/java/jdk1.8.0_141' >>/etc/profile
 echo 'export PATH=$JAVA_HOME/bin:$PATH' >>/etc/profile
 echo 'export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar' >>/etc/profile
 source /etc/profile
